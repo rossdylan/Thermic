09c7f0ca9e1f53e8add4ccc49f33b0083b5545de
Ross <rossdylan@csh.rit.edu>
2013-05-17 14:46:16 -0400

    Fixed up the setup.py file to support py2 and py3

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup, find_packages
+try:
+    from distutils.core import setup
+except ImportError:
+    from setuptools import setup
 
+
+readme = open('./README', 'r').read()
 setup(
     name="heat",
     description="Pure python interface for linux temperature sensing",
+    long_description=readme,
     version='0.1.0',
-    packages=find_packages(),
+    packages=['heat'],
     zip_safe=False
 )
